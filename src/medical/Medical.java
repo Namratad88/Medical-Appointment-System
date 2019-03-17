dcf6740181dfb960f65114b63d4e010412ab8eee
Admin <Admin@AW-LP>
2019-03-17 20:42:32 +0530

    Medical

---
+++ src/medical/Medical.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medical;
+
+/**
+ *
+ * @author Personal
+ */
+public class Medical {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Mainpage m=new Mainpage();
+    m.setVisible(true);
+            
+    }
+    
+}
